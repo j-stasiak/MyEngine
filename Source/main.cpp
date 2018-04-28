a4799181b51e4a725d5f0f0890729ba74874884f
Jakub Stasiak <jstasiak97@gmail.com>
2018-04-28 13:43:48 +0200

    Changed main to test TxtParser.

--- Source/main.cpp
+++ Source/main.cpp
@@ -3,6 +3,8 @@
 #include "Utility/Log/Log.h"
 #include <GL/glew.h>
 
+#include "Utility/TxtParser/TxtParser.h"
+
 ///
 /// Project: D.O.F.F
 /// Defend our fucking future
@@ -13,9 +15,17 @@ int main(void)
 	CLEAR_LOG; // Aby oczyœciæ plik logów po ostatnim u¿yciu.
 	LOG("Program started...");
 
+	TxtParser parser{ "res/settings/settings.txt" };
+
+	int value = parser.getProperty<int>("WINDOW", "width");
+
+	LOG_DEBUG(std::to_string(value));
+
+	/*
 	Game game;
 	game.runMainLoop();
-
+	*/
 	LOG("Program ended...");
+	std::system("pause>nul");
 	return 0;
 }
