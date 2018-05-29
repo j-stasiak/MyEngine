4815edf123907ba0594b2cbc5204d50b7bf955cd
Jakub Stasiak <jstasiak97@gmail.com>
2018-05-29 14:05:18 +0200

    Changed back to normal look of main.

--- Source/main.cpp
+++ Source/main.cpp
@@ -3,29 +3,20 @@
 #include "Utility/Log/Log.h"
 #include <GL/glew.h>
 
-#include "Utility/TxtParser/TxtParser.h"
+
 
 ///
 /// Project: D.O.F.F
-/// Defend our fucking future
 ///
 
 int main(void)
 {
 	CLEAR_LOG; // Aby oczyœciæ plik logów po ostatnim u¿yciu.
 	LOG("Program started...");
-
-	TxtParser parser{ "res/settings/settings.txt" };
-
-	int value = parser.getProperty<int>("WINDOW", "width");
-
-	LOG_DEBUG(std::to_string(value));
-
-	/*
+	
 	Game game;
 	game.runMainLoop();
-	*/
+	
 	LOG("Program ended...");
-	std::system("pause>nul");
 	return 0;
 }
